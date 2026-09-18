53b67f991f3352d0cef18928c57eee06d5ee2ddb
Kelvin <kinaskelvin1@gmail.com>
2026-09-18 20:30:06 -0300

    fix: added @RegisterForReflection in Output dto to Binary Compilation

--- src/main/java/com/domain/revenue/product/batch/productBatch/dtos/ProductBatchReceiveOutputDto.java
+++ src/main/java/com/domain/revenue/product/batch/productBatch/dtos/ProductBatchReceiveOutputDto.java
@@ -1,5 +1,8 @@
 package com.domain.revenue.product.batch.productBatch.dtos;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
 public record ProductBatchReceiveOutputDto(
         Long id
 ) {
